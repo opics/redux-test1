c782b87c387797d715b7d50b978bcc8cb0436e6c
opics <opics@mail.ru>
2019-07-07 22:58:28 +0300

    add payload

--- src/main.ts
+++ src/main.ts
@@ -17,13 +17,17 @@ const initialState = [];
 
 const reducer = (state = initialState, action) => {
   if (action.type === 'ADD') {
-    return [ ...state, 'Квартира'];
+    return [ ...state, `Квартира ${action.payload}`];
   }
   return [];
 };
+
+const rnd = () => Math.floor(Math.random() * 9) + 1;
 
 const store = createStore(reducer);
 
 store.subscribe(() => console.log(store.getState()));
 
-store.dispatch({type: 'ADD'});
+store.dispatch({type: 'ADD', payload: rnd()});
+
+store.dispatch({type: 'ADD', payload: rnd()});
