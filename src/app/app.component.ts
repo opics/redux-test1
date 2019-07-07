6823d60f21ce28ce8a662022263a582f24f5ad4e
opics <opics@mail.ru>
2019-07-07 22:59:26 +0300

    move code to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,24 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'redux-test1';
 }
+
+import { createStore } from 'redux';
+
+const initialState = [];
+
+const reducer = (state = initialState, action) => {
+  if (action.type === 'ADD') {
+    return [ ...state, `Квартира ${action.payload}`];
+  }
+  return [];
+};
+
+const rnd = () => Math.floor(Math.random() * 9) + 1;
+
+const store = createStore(reducer);
+
+store.subscribe(() => console.log(store.getState()));
+
+store.dispatch({type: 'ADD', payload: rnd()});
+
+store.dispatch({type: 'ADD', payload: rnd()});
--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,13 @@
-// import { enableProdMode } from '@angular/core';
-// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-// import { AppModule } from './app/app.module';
-// import { environment } from './environments/environment';
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
 
-// if (environment.production) {
-//   enableProdMode();
-// }
+if (environment.production) {
+  enableProdMode();
+}
 
-// platformBrowserDynamic().bootstrapModule(AppModule)
-//   .catch(err => console.error(err));
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
 
-import { createStore } from 'redux';
-
-const initialState = [];
-
-const reducer = (state = initialState, action) => {
-  if (action.type === 'ADD') {
-    return [ ...state, `Квартира ${action.payload}`];
-  }
-  return [];
-};
-
-const rnd = () => Math.floor(Math.random() * 9) + 1;
-
-const store = createStore(reducer);
-
-store.subscribe(() => console.log(store.getState()));
-
-store.dispatch({type: 'ADD', payload: rnd()});
-
-store.dispatch({type: 'ADD', payload: rnd()});
