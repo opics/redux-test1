847143469cd93e895f84cf8ce37823440de3c10d
opics <opics@mail.ru>
2019-07-07 22:32:52 +0300

    add non-redux reducer

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,31 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+// import { enableProdMode } from '@angular/core';
+// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+// import { AppModule } from './app/app.module';
+// import { environment } from './environments/environment';
 
-if (environment.production) {
-  enableProdMode();
-}
+// if (environment.production) {
+//   enableProdMode();
+// }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+// platformBrowserDynamic().bootstrapModule(AppModule)
+//   .catch(err => console.error(err));
+
+const initialState = [];
+
+const reducer = (state = initialState, action) => {
+  if (action.type === 'ADD') {
+    return [ ...state, 'Квартира'];
+  }
+  return [];
+};
+
+let state = reducer(undefined, {});
+
+state = reducer(state, { type: 'ADD'});
+
+console.log(state);
+
+state = reducer(state, { type: 'ADD'});
+
+console.log(state);
